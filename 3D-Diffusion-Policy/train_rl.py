a19b9f6459a5aee0ba615bbdf8788610d25a98f9
Mei <mei.hao@yale.edu>
2024-07-31 16:34:05 -0400

    Fixed errors with training rl with stablebaselines using metaworld envs

--- 3D-Diffusion-Policy/train_rl.py
+++ 3D-Diffusion-Policy/train_rl.py
@@ -7,7 +7,7 @@ from stable_baselines3.common.env_checker import check_env
 
 from diffusion_policy_3d.env import MetaWorldEnv
 from gymnasium.wrappers import EnvCompatibility
-
+from cprint import *
 def main(args):
     env_name = args.env_name
 
@@ -29,6 +29,7 @@ def main(args):
     env = MetaWorldEnv(env_name, device="cuda:0", use_point_crop=True)
     env = EnvCompatibility(env, 'none')
     check_env(env)
+    cprint.info('Env check passed')
 
     model = PPO("MultiInputPolicy", env, verbose=1)
     model.learn(total_timesteps=args.num_timesteps)
@@ -38,14 +39,14 @@ def main(args):
     print(f"Mean reward: {mean_reward} +/- {std_reward}")
 
     # eval
-    obs = env.reset()
-
+    obs = env.reset()[0]
     for _ in range(1000):
         action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
+        #obs, rewards, dones, info = env.step(action) #this is gym format
+        obs, reward, terminated, truncated, info = env.step(action) #this is gymnasium
         env.render()
-        if dones:
-            obs = env.reset()
+        if terminated:
+            obs = env.reset()[0]
 
     env.close()
 
@@ -55,7 +56,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--env_name', type=str, default='hammer')
     parser.add_argument('--num_timesteps', type=int, default=1000)
-    parser.add_argument('--model_save_dir', type=str, default="/data/outputs_rl/" )
+    parser.add_argument('--model_save_dir', type=str, default="./data/outputs_rl/" )
 
     args = parser.parse_args()
     main(args)
