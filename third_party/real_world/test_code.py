Fix bug for calculating rotation matrix differences

third_party/real_world/test_code.py
@@ -20,7 +20,7 @@ from diffusion_policy_3d.env.real_world import CONSTANTS
 import keyboard
 from icecream import ic 
 
-from rotation_transformer import RotationTransformer
+from tqdm import tqdm
 
 # # Example usage of the klampt.model.trajectory library
 
@@ -52,15 +52,34 @@ from rotation_transformer import RotationTransformer
 # print("Loaded trajectory milestones:", loaded_traj.milestones)
 
 # Create two rotation matrices using scipy.spatial.transform.Rotation
-rotation1 = Rotation.from_euler('xyz', [45, 45, 45], degrees=True).as_matrix()
-rotation2 = Rotation.from_euler('xyz', [90, 90, 90], degrees=True).as_matrix()
+rotation1 = Rotation.from_euler('xyz', [13, 52, 53], degrees=True).as_matrix()
+rotation2 = Rotation.from_euler('xyz', [7, 43, 6], degrees=True).as_matrix()
 
 # Flatten the rotation matrices to lists, column major form
 r1 = rotation1.T.flatten().tolist()
 r2 = rotation2.T.flatten().tolist()
 
-r1_vec = so3.rotation_vector(r1)
+# Concatenate r1 and r2 into an array of shape (n, 9)
+rotations = np.array([r1, r2])
+ic(rotations)
 
-rotation_transformer = RotationTransformer('axis_angle', 'rotation_6d')
+rotation_matrices = rotations.reshape(-1, 3, 3, order='F')
+ic(rotation_matrices)
+rotation_matrices = rotation_matrices.astype(np.float32)
+ic(rotation_matrices)
+rotation_diff_matrices = np.array([r2 @ np.linalg.inv(r1) for r1, r2 in tqdm(zip(rotation_matrices[:-1], rotation_matrices[1:]), total=len(rotation_matrices) - 1)], dtype=np.float32)
+ic(rotation_diff_matrices)
 
-ic(r1_vec)
+reverse = np.linalg.inv(rotation_diff_matrices) @ rotation2
+ic(reverse)
+ic(rotation1)
+
+# Convert the rotation difference matrices to Euler angles
+rotation_diff_euler = np.array([Rotation.from_matrix(r).as_euler('xyz', degrees=True) for r in rotation_diff_matrices], dtype=np.float32)
+ic(rotation_diff_euler)
+
+ic(rotation_diff_matrices)
+rotation_diff_matrices = rotation_diff_matrices[:, :-1, :]  # Remove the last row of each 3 by 3 rotation matrix
+ic(rotation_diff_matrices)
+rotation_diff = rotation_diff_matrices.reshape(-1, 6)
+ic(rotation_diff)